680f2d9730400debc0db11c4859c3e466ffa2e69
AnhMeo <u799696294@byui.edu>
2025-05-10 20:05:33 +0700

    reworked client to also incorporate threading

--- ChattyCat_Client.py
+++ ChattyCat_Client.py
@@ -1,16 +1,30 @@
 import socket
 import threading
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def receive_messages(sock):
+    while True:
+        try:
+            msg = sock.recv(1024).decode('utf-8')
+            if not msg:
+                break
+            print(f"Server: {msg}")
+        except:
+            break
 
-client.connect(("localhost", 9999))
+def start_client():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((HOST, PORT))
 
-done = False
+    threading.Thread(target = receive_messages, args = (client,), daemon = True).start()
 
-while not done:
-    client.send(input("message: ").encode('utf-8'))
-    msg = client.recv(1024).decode('utf-8')
-    if msg == "quit":
-        done = True
-    else:
-        print(msg)
+    while True:
+        msg = input("You: ")
+        if msg.lower() == "quit":
+            client.send(msg.encode('utf-8'))
+            break
+        client.send(msg.encode('utf-8'))
+    
+    client.close()
+
+if __name__ == "__main__":
+    start_client()
